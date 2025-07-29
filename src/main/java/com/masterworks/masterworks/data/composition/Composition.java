949e047274378632fbc9001233bffbea1a48d295
Yonatan Eavri <yonatan.eavri@gmail.com>
2025-07-29 22:15:44 +0300

    Renamed Part -> PartDefinition

--- src/main/java/com/masterworks/masterworks/data/composition/Composition.java
+++ src/main/java/com/masterworks/masterworks/data/composition/Composition.java
@@ -7,11 +7,11 @@ import com.masterworks.masterworks.util.Expression;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
-public record Composition(int tier, List<Part> parts, Map<Stat, Expression> properties) {
+public record Composition(int tier, List<PartDefinition> parts, Map<Stat, Expression> properties) {
 
     public static final Codec<Composition> CODEC = RecordCodecBuilder.create(instance -> instance
             .group(Codec.INT.fieldOf("tier").forGetter(Composition::tier),
-                    Codec.list(Part.CODEC).fieldOf("parts").forGetter(Composition::parts),
+                    Codec.list(PartDefinition.CODEC).fieldOf("parts").forGetter(Composition::parts),
                     Codec.simpleMap(Stat.CODEC, Expression.CODEC, Registries.STAT)
                             .fieldOf("properties").forGetter(Composition::properties))
             .apply(instance, Composition::new));
---
+++ src/main/java/com/masterworks/masterworks/data/composition/PartDefinition.java
@@ -0,0 +1,16 @@
+package com.masterworks.masterworks.data.composition;
+
+import java.util.Optional;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.resources.ResourceLocation;
+
+public record PartDefinition(Optional<ResourceLocation> shape,
+        Optional<ResourceLocation> requires) {
+
+    public static final Codec<PartDefinition> CODEC = RecordCodecBuilder.create(instance -> instance
+            .group(ResourceLocation.CODEC.optionalFieldOf("shape").forGetter(PartDefinition::shape),
+                    ResourceLocation.CODEC.optionalFieldOf("requires")
+                            .forGetter(PartDefinition::requires))
+            .apply(instance, PartDefinition::new));
+}
