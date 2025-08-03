e3aa1ef0f5e6ce37e7512f48ae8c566ee5e601fb
Yonatan Eavri <yonatan.eavri@gmail.com>
2025-08-03 23:57:57 +0300

    Added template items to creative mode tabs

--- src/main/java/com/masterworks/masterworks/item/TemplateItem.java
+++ src/main/java/com/masterworks/masterworks/item/TemplateItem.java
@@ -1,7 +1,11 @@
 package com.masterworks.masterworks.item;
 
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.registries.DeferredHolder;
+import java.util.function.Supplier;
 import com.masterworks.masterworks.Masterworks;
 
 /**
@@ -15,8 +19,6 @@ public class TemplateItem extends Item {
     private TemplateItem(Item.Properties properties) {
         super(properties);
     }
-
-
 
     public static final DeferredHolder<Item, TemplateItem> HANDLE_TEMPLATE =
             Masterworks.ITEMS.registerItem("rod_template", TemplateItem::new);
@@ -36,4 +38,17 @@ public class TemplateItem extends Item {
     public static final DeferredHolder<Item, TemplateItem> SWORD_TEMPLATE =
             Masterworks.ITEMS.registerItem("sword_template", TemplateItem::new);
 
+
+    public static final Supplier<CreativeModeTab> TEMPLATES_TAB =
+            Masterworks.CREATIVE_MODE_TABS.register("templates", () -> CreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup." + Masterworks.MOD_ID + ".templates"))
+                    .icon(() -> new ItemStack(PICKAXE_TEMPLATE.get()))
+                    .displayItems((params, output) -> {
+                        output.accept(HANDLE_TEMPLATE.get());
+                        output.accept(BINDING_TEMPLATE.get());
+                        output.accept(PICKAXE_HEAD_TEMPLATE.get());
+                        output.accept(PICKAXE_TEMPLATE.get());
+                        output.accept(SWORD_BLADE_TEMPLATE.get());
+                        output.accept(SWORD_TEMPLATE.get());
+                    }).build());
 }
